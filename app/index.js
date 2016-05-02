33dac7183dce431031b809be929b11d9be297bf3
Sawako Ishida <sawako.yoriko@gmail.com>
2016-05-02 15:41:17 -0700

    made index.js as an entry point and removed all my codes into gmap-controller.js

--- app/index.js
+++ app/index.js
@@ -1,29 +1,4 @@
-const angular = require('angular');
-const app = angular.module('myApp', []);
+
+
 require(__dirname + '/directives/gmap-directive.js')(app);
-
-app.controller('mapController', ['$window', function($window){
-  var pikePlace = {lat: 47.608953, lng: -122.341099};
-  var bellevueMall = {lat: 47.616591, lng: -122.198797};
-  this.user = {};
-  this.startingPoint = {
-    street: '',
-    city: '',
-    state: ''
-  };
-  this.destination  = {
-    street: '',
-    city: '',
-    state: ''
-  };;
-
-  this.initialize = function(){
-    $window.Gmap.initMap(pikePlace, bellevueMall);
-  }
-
-  this.search = function(){
-    $window.Gmap.convertAddress()
-    console.log(angular.toJson(this.startingPoint));
-    console.log(angular.toJson(this.destination));
-  }
-}]);
+require(__dirname + '/controller/gmap-controller.js')(app);
