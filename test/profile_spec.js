1fd854e92a1d788da1de890c70c98663a0e38d9b
lwenke01 <lwenke@gmail.com>
2016-05-05 19:55:52 -0700

    testing set up

---
+++ test/profile_spec.js
@@ -0,0 +1,16 @@
+require('../app/controller/profile-controller.js');
+const angular = require('angular');
+require('angular-mocks');
+
+describe('should test something'), ()=> {
+  var profileController;
+  it('should test', ()=>{
+    expect(true).toBe(true);
+  });
+  beforeEach(angular.mock.module('rideShareApp'))
+  beforeEach(angular.mock.inject(function($controller){
+    p
+
+  }))
+
+}
